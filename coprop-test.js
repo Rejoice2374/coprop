7eac9f7a2230614e682f63fc245fd50b4a9a77bc
volkovasystems <richeve.bebedor@gmail.com>
2017-04-21 23:17:49 +0800

    Finished coprop procedure.

---
+++ coprop-test.js
@@ -0,0 +1,7 @@
+
+const coprop = require( "./coprop.js" );
+
+var source = { "hello": "world" };
+var target = { "yeah": "world" };
+
+console.log( coprop( "hello", source, target ) );
---
+++ coprop.support.js
@@ -0,0 +1,99 @@
+"use strict";
+
+/*;
+              	@module-license:
+              		The MIT License (MIT)
+              		@mit-license
+              
+              		Copyright (@c) 2017 Richeve Siodina Bebedor
+              		@email: richeve.bebedor@gmail.com
+              
+              		Permission is hereby granted, free of charge, to any person obtaining a copy
+              		of this software and associated documentation files (the "Software"), to deal
+              		in the Software without restriction, including without limitation the rights
+              		to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+              		copies of the Software, and to permit persons to whom the Software is
+              		furnished to do so, subject to the following conditions:
+              
+              		The above copyright notice and this permission notice shall be included in all
+              		copies or substantial portions of the Software.
+              
+              		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+              		IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+              		FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+              		AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+              		LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+              		OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+              		SOFTWARE.
+              	@end-module-license
+              
+              	@module-configuration:
+              		{
+              			"package": "coprop",
+              			"path": "coprop/coprop.js",
+              			"file": "coprop.js",
+              			"module": "coprop",
+              			"author": "Richeve S. Bebedor",
+              			"eMail": "richeve.bebedor@gmail.com",
+              			"repository": "https://github.com/volkovasystems/coprop.git",
+              			"global": true
+              		}
+              	@end-module-configuration
+              
+              	@module-documentation:
+              		Copy property and descriptors.
+              	@end-module-documentation
+              
+              	@include:
+              		{
+              			"falzy": "falzy",
+              			"protype": "protype"
+              		}
+              	@end-include
+              */var _defineProperty = require("babel-runtime/core-js/object/define-property");var _defineProperty2 = _interopRequireDefault(_defineProperty);var _getOwnPropertyDescriptor = require("babel-runtime/core-js/object/get-own-property-descriptor");var _getOwnPropertyDescriptor2 = _interopRequireDefault(_getOwnPropertyDescriptor);function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}
+
+var falzy = require("falzy");
+var protype = require("protype");
+
+var coprop = function coprop(property, source, target) {
+	/*;
+                                                        	@meta-configuration:
+                                                        		{
+                                                        			"property:required": [
+                                                        				"number",
+                                                        				"string",
+                                                        				"symbol"
+                                                        			],
+                                                        			"source:required": "*",
+                                                        			"target:required": "*"
+                                                        		}
+                                                        	@end-meta-configuration
+                                                        */
+
+	if (falzy(property) || !protype(property, NUMBER + STRING + SYMBOL)) {
+		throw new Error("invalid property");
+	}
+
+	if (falzy(source)) {
+		throw new Error("invalid source");
+	}
+
+	if (falzy(target)) {
+		throw new Error("invalid target");
+	}
+
+	try {
+		var descriptor = (0, _getOwnPropertyDescriptor2.default)(source, property);
+
+		(0, _defineProperty2.default)(target, property, descriptor);
+
+		return target;
+
+	} catch (error) {
+		throw new Error("cannot copy property, " + error.stack);
+	}
+};
+
+module.exports = coprop;
+
+//# sourceMappingURL=coprop.support.js.map
